7cec10b4136074a9f974e76ef75c72be96a300e9
somw <somw@qq.com>
2019-04-29 16:57:04 +0800

    20190429

--- 爬虫第3关/6.py
+++ 爬虫第3关/6.py
@@ -2,25 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import quote
 #quote()函数，可以帮我们把内容转为标准的url格式，作为网址的一部分打开
-res = requests.get('https://www.ygdy8.com')
-res.encoding = 'gbk'
+name = input('输入电影名：')
+gbkname=name.encode('gbk')
+url = 'http://s.ygdy8.com/plus/so.php?typeid=1&keyword='+quote(gbkname)
+#print(url)
+
+res = requests.get(url)
+res.encoding='gbk'
 html = res.text
 soup = BeautifulSoup(html,'html.parser')
-aa = soup.find_all('div',class_='searchl')
+bb = soup.find(class_='co_content8').find_all('table')
+#print(bb)
 
-
-names = soup.find('input')['value'][15:]
-
-name = input('输入电影名：%s' % names)
-
-
-url = 'http://s.ygdy8.com/plus/so.php?typeid=1&keyword='+quote(name)
-
-
-res1 = requests.get(url)
-html1 = res1.text
-soup1 = BeautifulSoup(html,'html.parser')
-bb = soup1.find_all('div',class_='co_content8')
-
-for b in bb:
-    print(bb[b].find('a')['href'])
+if bb:
+    bb = bb[0].find('a')['href']
+    bburl = 'https://www.ygdy8.com/'+bb
+    res1 = requests.get(bburl)
+    res1.encoding = 'gbk'
+    bb_soup = BeautifulSoup(res1.text,'html.parser')
+    urldownload = bb_soup.find('div',id="Zoom").find('span').find('table').find('a')['href']
+    print(urldownload) 
+else:
+    print('没有' + name)
